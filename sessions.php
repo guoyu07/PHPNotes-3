a0083ac792b73ac0cde5cd38a94ee54a9352f407
Vivek Ratkalkar <vratkalkar@live.com>
2016-03-10 16:32:39 -0500

    Add PHP SecureSessionHandler class example

---
+++ sessions.php
@@ -0,0 +1,47 @@
+<?php
+
+class SecureSessionHandler extends SessionHandler {
+	protected $key, $name, $cookie;
+
+	public function __construct($key, $name = 'MY_SESSION', $cookie = []){
+		$this->key = $key;
+		$this->name = $name;
+		$this->cookie = $cookie;
+
+		$this->cookie += [
+			'lifetime' => 0,
+			'path'     => ini_get('session.cookie_path'),
+			'domain'   => ini_get('session.cookie_domain'),
+			'secure'   => isset($_SERVER['HTTPS']),
+			'httponly' => true
+		];
+
+		$this->setup();
+	}
+
+	protected function setup(){
+		ini_set('session.use_cookies', 1);
+		ini_set('session.use_only_cookies', 1);
+
+		session_name($this->name);
+
+		session_set_cookie_params(
+			$this->cookie['lifetime'], $this->cookie['path'],
+			$this->cookie['domain'], $this->cookie['secure'],
+			$this->cookie['httponly']
+		);
+	}
+}
+
+
+$obj = new SecureSessionHandler("hello");
+
+print_r($obj);
+
+
+
+
+
+
+
+?>
